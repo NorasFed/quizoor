f20d14f4349e7fdc5823721a7716ab51bd8e730f
Nouri Atchabao <norasfed@gmail.com>
2019-11-12 10:49:48 +0000

    Initial commit

---
+++ js/detects.js
@@ -0,0 +1,2 @@
+/*! Quizoor v0.0.1 | (c) 2019 */
+console.log('detects.js file loaded');
---
+++ js/main.polyfills.js
@@ -0,0 +1,178 @@
+/*! Quizoor v0.0.1 | (c) 2019 */
+
+/*
+================================================================================
+
+  "FAQ" section
+
+================================================================================
+*/
+
+const FAQ = (function() {
+
+    const togglePanel = function(event) {
+      const targetElem = event.target;
+  
+      if (targetElem.classList.contains('lp-faq-item-header__btn')) {
+        const btnElem = targetElem;
+        const faqItemElem = btnElem.closest('.lp-faq-item');
+  
+        if (!faqItemElem.classList.contains('js-expanded')) {
+          faqItemElem.classList.add('js-expanded');
+          btnElem.setAttribute('aria-expanded', true);
+        } else {
+          faqItemElem.classList.remove('js-expanded');
+          btnElem.setAttribute('aria-expanded', false);
+        }
+      }
+    };
+  
+    const initEventListeners = function() {
+      if (document.querySelectorAll('.lp-faq-item')) {
+        const faqItemsList = document.querySelectorAll('.lp-faq-item');
+  
+        Array.from(faqItemsList, (item) => {
+          item.addEventListener('click', togglePanel);
+        });
+      }
+    };
+  
+    const init = function() {
+      initEventListeners();
+    };
+  
+    return {
+      init: init
+    };
+  
+  })();
+  
+  FAQ.init();
+  
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.querySelector("#my-bar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+var x, i, j, selElmnt, a, b, c;
+            /*look for any elements with the class "custom-select":*/
+            x = document.getElementsByClassName("custom-select");
+            for (i = 0; i < x.length; i++) {
+              selElmnt = x[i].getElementsByTagName("select")[0];
+              /*for each element, create a new DIV that will act as the selected item:*/
+              a = document.createElement("DIV");
+              a.setAttribute("class", "select-selected");
+              a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+              x[i].appendChild(a);
+              /*for each element, create a new DIV that will contain the option list:*/
+              b = document.createElement("DIV");
+              b.setAttribute("class", "select-items select-hide");
+              for (j = 1; j < selElmnt.length; j++) {
+                /*for each option in the original select element,
+                create a new DIV that will act as an option item:*/
+                c = document.createElement("DIV");
+                c.innerHTML = selElmnt.options[j].innerHTML;
+                c.addEventListener("click", (function(e) {
+                    /*when an item is clicked, update the original select box,
+                    and the selected item:*/
+                    var y, i, k, s, h;
+                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                    h = this.parentNode.previousSibling;
+                    for (i = 0; i < s.length; i++) {
+                      if (s.options[i].innerHTML == this.innerHTML) {
+                        s.selectedIndex = i;
+                        h.innerHTML = this.innerHTML;
+                        y = this.parentNode.getElementsByClassName("same-as-selected");
+                        for (k = 0; k < y.length; k++) {
+                          y[k].removeAttribute("class");
+                        }
+                        this.setAttribute("class", "same-as-selected");
+                        break;
+                      }
+                    }
+                    h.click();
+                }));
+                b.appendChild(c);
+              }
+              x[i].appendChild(b);
+              a.addEventListener("click", (function(e) {
+                  /*when the select box is clicked, close any other select boxes,
+                  and open/close the current select box:*/
+                  e.stopPropagation();
+                  closeAllSelect(this);
+                  this.nextSibling.classList.toggle("select-hide");
+                  this.classList.toggle("select-arrow-active");
+                }));
+            }
+            function closeAllSelect(elmnt) {
+              /*a function that will close all select boxes in the document,
+              except the current select box:*/
+              var x, y, i, arrNo = [];
+              x = document.getElementsByClassName("select-items");
+              y = document.getElementsByClassName("select-selected");
+              for (i = 0; i < y.length; i++) {
+                if (elmnt == y[i]) {
+                  arrNo.push(i)
+                } else {
+                  y[i].classList.remove("select-arrow-active");
+                }
+              }
+              for (i = 0; i < x.length; i++) {
+                if (arrNo.indexOf(i)) {
+                  x[i].classList.add("select-hide");
+                }
+              }
+            }
+            /*if the user clicks anywhere outside the select box,
+            then close all select boxes:*/
+            document.addEventListener("click", closeAllSelect);
+document.addEventListener('click', (function (event) {
+	if (!event.target.matches('#click-me')) return;
+	alert('You clicked me!');
+}), false);
+function openTab(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
+
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
